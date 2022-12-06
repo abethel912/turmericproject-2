446138558c2d3290d069c3cd4e01390e171849ef
abethel912 <abethel180@gmail.com>
2022-12-05 20:17:08 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+// import 
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
+
+// create express app
+const app = express();
+
+// establish mongo connection
+mongoose.connect(process.env.DATABASE_URL);
+
+// mongoose connection events
+mongoose.connection.on("open", () => console.log("Connected to Mongo"));
+mongoose.connection.on("close", () => console.log("Disconnected from Mongo"));
+mongoose.connection.on("error", (error) => console.log(error));
+
+// register my middleware
+app.use(morgan("dev"));
+app.use("/static", express.static("public"));
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride("_method"))
+
+// routes and routers
+app.get("/", (req, res) => {
+  res.send("<h1>Server is working </h1>")
+})
+
+// start the server
+const PORT = process.env.port || 3000
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`))
