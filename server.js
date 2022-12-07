5d8390dfcf08c1973fd43df9251989547de57058
abethel912 <abethel180@gmail.com>
2022-12-06 20:26:17 -0500

    Created drinks partials and user folders inside views

--- server.js
+++ server.js
@@ -3,24 +3,29 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const methodOverride = require("method-override");
-const mongoose = require("mongoose");
+const DrinksRouter = require('./controllers/drinks')
+const UserRouter = require('./controllers/user')
+// const mongoose = require("mongoose");
+
 
 // create express app
 const app = express();
 
-// establish mongo connection
-mongoose.connect(process.env.DATABASE_URL);
+// // establish mongo connection
+// mongoose.connect(process.env.DATABASE_URL);
 
-// mongoose connection events
-mongoose.connection.on("open", () => console.log("Connected to Mongo"));
-mongoose.connection.on("close", () => console.log("Disconnected from Mongo"));
-mongoose.connection.on("error", (error) => console.log(error));
+// // mongoose connection events
+// mongoose.connection.on("open", () => console.log("Connected to Mongo"));
+// mongoose.connection.on("close", () => console.log("Disconnected from Mongo"));
+// mongoose.connection.on("error", (error) => console.log(error));
 
-// register my middleware
-app.use(morgan("dev"));
-app.use("/static", express.static("public"));
-app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride("_method"))
+// // register my middleware
+// app.use(morgan("dev"));
+// app.use("/static", express.static("public"));
+// app.use(express.urlencoded({ extended: true }))
+// app.use(methodOverride("_method"))
+// app.use('/drinks', DrinksRouter)
+// app.use('/user', UserRouter)
 
 // routes and routers
 app.get("/", (req, res) => {
